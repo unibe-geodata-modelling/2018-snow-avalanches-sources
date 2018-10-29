a5a8732a121a9ea056fbcb8d8755d688dc4f4b7b
Unknown <thalia.bertschinger@students.unibe.ch>
2018-10-29 09:23:23 +0100

    Potential Release Areas
    First part of the script

---
+++ 20181025_PRA.py
@@ -0,0 +1,195 @@
+# Script for the automated mapping of potential snow avalanche release areas
+# Thalia Bertschinger
+# Seminar Geodata Analysis and Modelling, Spring Semester 2018
+# The model creates polygons of potential snow avalanche release areas
+
+# imports
+import arcpy
+
+if arcpy.CheckExtension("Spatial") == "Available":
+    arcpy.CheckOutExtension('Spatial')
+else:
+    print "no spatial analyst license available"
+
+#**************************************************************************
+# ENVIRONMENT variables - set workspace and names of input files
+#**************************************************************************
+
+# set environment and workspace and create gdb
+preworkspace = "U:/Seminar_Modellieren/20181018_Test_Model"
+tempdir = "C:/temp"
+arcpy.env.overwriteOutput = True
+# Create File GDB
+gdb = "20181025_Model_PRA.gdb"
+arcpy.CreateFileGDB_management(preworkspace, gdb, "CURRENT")
+myworkspace = preworkspace+"/"+gdb
+print "Workspace: " + myworkspace
+arcpy.env.workspace = myworkspace
+
+# input data
+pre_dem = arcpy.Raster(preworkspace+"/"+"dem.tif")
+
+# Resample dem to 5 x 5 cell size
+dem_5m = myworkspace+"/"+"dem_5m"
+cell_size = 5
+resample_method = "BILINEAR"
+arcpy.Resample_management(pre_dem, dem_5m, cell_size, resample_method)
+
+dem = arcpy.Raster(dem_5m)
+arcpy.env.cellSize = dem
+arcpy.env.extent = dem
+arcpy.env.snapRaster = dem
+
+#**************************************************************************
+# create temporary files if wished
+#**************************************************************************
+
+# slope = tempdir+"/"+"slope.tif" # temporary file for the slope analysis
+# aspect = tempdir+"/"+"aspect.tif" # temporary file for the aspect analysis
+# aspect_classes = tempdir+"/"+"aspect_classes.tif" # temporary file for the aspect classes (reclassified aspect)
+# curvature = tempdir+"/"+"curvature.tif" # temporary file for the curvature analysis
+plan_curvature = tempdir+"/"+"plan_curvature.tif"  # temporary file for the plan curvature
+profile_curvature = tempdir+"/"+"profile_curvature.tif"  # temporary file for the plan curvature
+
+#**************************************************************************
+# start analyses
+#**************************************************************************
+
+# create slope
+out_slope = "DEGREE"
+z_slope = 1
+outSlope = arcpy.sa.Slope(dem, out_slope, z_slope)
+# outSlope.save(slope)
+
+# create aspect
+outAspect = arcpy.sa.Aspect(dem)
+# outAspect.save(aspect)
+# reclassify aspect to get 9 aspect classes
+reclass_field = "VALUE"
+range_aspect_classes = arcpy.sa.RemapRange([[-1,0,1],
+                         [0,22.500000,2],
+                         [22.500000,67.500000,3],
+                         [67.500000,112.500000,4],
+                         [112.500000,157.500000,5],
+                         [157.500000,202.500000,6],
+                         [202.500000,247.500000,7],
+                         [247.500000,292.500000,8],
+                         [292.500000,337.500000,9],
+                         [337.500000,360,2]])
+outAspect_classes = arcpy.sa.Reclassify(outAspect, reclass_field, range_aspect_classes)
+# outAspect_classes.save(aspect_classes)
+
+# create curvature
+z_curvature = 1
+outCurvature = arcpy.sa.Curvature(dem, z_curvature, profile_curvature, plan_curvature)
+# outCurvature.save(curvature)
+# plan curvature is used later
+planCurvature = arcpy.Raster(plan_curvature)
+
+# identify slopes in a certain range
+min_slope = 30
+max_slope = 60
+conSlope = arcpy.sa.Con((outSlope >= min_slope) & (outSlope <= max_slope), 1)
+# conSlope.save(tempdir+"/"+"conslope.tif")
+
+# set cells with a plan curvature bigger than a certain threshold to NoData (first assign them the value 2)
+max_curv = 6
+conCurv = arcpy.sa.Con(planCurvature > max_curv, 2, conSlope)
+PRA1 = arcpy.sa.Con(conCurv == 1, 1)
+
+# assign aspect values to the cells in PRA1
+PRA1_aspect = arcpy.sa.Con(PRA1 == 1, outAspect_classes)
+# PRA1_aspect.save(tempdir+"/"+"PRA1_apect.tif")
+
+#**************************************************************************
+# convert raster to polygon and simplify them
+#**************************************************************************
+
+# convert raster to polygon
+PRA1_poly = myworkspace+"/"+"PRA1"  # is saved in gdb to have the area automatically added in the attribute table
+arcpy.RasterToPolygon_conversion(PRA1_aspect, PRA1_poly, "SIMPLIFY", "Value")
+
+# convert multipart to singlepart features
+PRA1_single = myworkspace+"/"+"PRA1_single"
+arcpy.MultipartToSinglepart_management(PRA1_poly, PRA1_single)
+
+# eliminate polygon parts with an area smaller than a certain threshold
+PRA2_1 = myworkspace+"/"+"PRA2_1"
+PRA2_con = "AREA"
+area_tresh = "5000 SquareMeters"
+perc = 0  # Eliminate parts smaller than this percentage of a feature's total outer area.
+option = "ANY"
+arcpy.EliminatePolygonPart_management(PRA1_single, PRA2_1, PRA2_con, area_tresh, perc, option)
+
+# make a feature layer and select polygons with an area smaller than 5000 m^2 and merge them with neighbouring polygons
+# create a loop that is executed as long as polygons smaller than 5000 m^2 are present
+# create a list where the number of polygons smaller than 5000 m2 will be added
+small_polygons = []
+count = 1
+while count >= 1:
+    feature_layer = "PRA_layer"+str(count)
+    in_feature = myworkspace+"/"+"PRA2_"+str(count)
+    arcpy.MakeFeatureLayer_management(in_feature, feature_layer)  # make a feature layer
+    selection = "NEW_SELECTION"
+    merge_tresh = "Shape_Area < 5000"
+    # select polygons smaller than 5000 m^2
+    arcpy.SelectLayerByAttribute_management(feature_layer, selection, merge_tresh)
+    sel_polygons = int(arcpy.GetCount_management(feature_layer).getOutput(0))
+    small_polygons.append(sel_polygons)
+    print('{} polygons are smaller than 5000 m^2'.format(sel_polygons))
+    while sel_polygons > 0:
+        if len(small_polygons) == 1:
+            count += 1
+            PRA2_elim = myworkspace + "/" + "PRA2_" + str(count)
+            merge_how = "LENGTH"  # The neighboring polygon is the one with the longest shared border.
+            # Merges a selected polygon with a neighboring unselected polygon by dropping the shared border.
+            # merge the small polygons with neighbouring big ones
+            arcpy.Eliminate_management(feature_layer, PRA2_elim, merge_how)
+            break
+        elif len(small_polygons) > 1:
+            if small_polygons[- 1] != small_polygons[- 2]:
+                count += 1
+                PRA2_elim = myworkspace + "/" + "PRA2_"+str(count)
+                merge_how = "LENGTH"  # The neighboring polygon is the one with the longest shared border.
+                # Merges a selected polygon with a neighboring unselected polygon by dropping the shared border.
+                # merge the small polygons with neighbouring big ones
+                arcpy.Eliminate_management(feature_layer, PRA2_elim, merge_how)
+                break
+            else:
+                count = 0
+                break
+        else:
+            count = 0
+            break
+    else:
+        count = 0
+        break
+
+
+
+
+#**************************************************************************
+# delete all files in the temp folder
+#**************************************************************************
+
+import os
+for the_file in os.listdir(tempdir):
+    file_path = os.path.join(tempdir, the_file)
+    try:
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+    except Exception as e:
+        print(e)
+
+#**************************************************************************
+# delete all files in the gdb except the real PRA file
+#**************************************************************************
+
+# fuktioniert noch nicht!!!!
+for the_file in os.listdir(myworkspace):
+    file_path = os.path.join(myworkspace, the_file)
+    try:
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+    except Exception as e:
+        print(e)
